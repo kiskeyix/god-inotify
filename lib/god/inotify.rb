9fa0fb6a8e596c02d1aeae4fa3de72e306c3f6d6
Luis Mondesi <lemsx1@gmail.com>
2015-09-05 05:04:56 -0400

    initial commit

---
+++ lib/god/inotify.rb
@@ -0,0 +1,7 @@
+require "god/inotify/version"
+
+module God
+  module Inotify
+    # Your code goes here...
+  end
+end
---
+++ lib/god/inotify/version.rb
@@ -0,0 +1,5 @@
+module God
+  module Inotify
+    VERSION = "0.1.0"
+  end
+end
