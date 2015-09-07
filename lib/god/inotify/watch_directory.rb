57798d0fbd12a1efaca1efc5ce36d2f6b58be01d
Luis Mondesi <lemsx1@gmail.com>
2015-09-06 21:54:59 -0400

    basic structure for class

--- lib/god/inotify/watch_directory.rb
+++ lib/god/inotify/watch_directory.rb
@@ -1,7 +1,36 @@
+require 'rb-inotify'
+
 module God
   module Inotify
     class WatchDirectory
       def initialize(opts={})
+        @notifier = INotify::Notifier.new
+        @god = opts[:god]
+      end
+      # runs a loop to watch over all configured directories for changes
+      def watch_directories
+        return unless @god.responds_to? :watch and @god.responds_to? :unwatch
+
+        @notifier.watch "/etc/god/processes", :create,
+          :moved_to, :moved_from, :delete do |event|
+          case event.name
+          when "delete"
+            @god.unwatch do
+            end
+          else
+            @god.watch do
+            end
+          end
+        end
+      end
+      def run
+        @notifier.run
+      end
+      def process
+        @notifier.process
+      end
+      def stop
+        @notifier.stop
       end
     end
   end
