30ebe18959f9be279042e808205a991044a71814
Luis Mondesi <lemsx1@gmail.com>
2015-09-08 05:11:27 +0000

    basic skeleton for handling watches

--- lib/god/inotify/watch_process.rb
+++ lib/god/inotify/watch_process.rb
@@ -2,10 +2,30 @@ module God
   module Inotify
     class NameRequired < StandardError
     end
+    class UnknownFilename < StandardError
+    end
     class WatchProcess
       def initialize(opts={})
         @name = opts[:name]
         raise NameRequired, "+name+ is a required attribute" unless @name.nil?
+      end
+      def watch(file)
+        if file =~ /\.god$/
+          # just load
+        elsif file =~ /\.yml$/
+          # parse and create watch
+        else
+          raise UnknownFilename, "File #{file} is not supported. Should be .god or .yml"
+        end
+      end
+      def unwatch(file)
+        if file =~ /\.god$/
+          # just unload this process
+        elsif file =~ /\.yml$/
+          # parse and unwatch this process
+        else
+          raise UnknownFilename, "File #{file} is not supported. Should be .god or .yml"
+        end
       end
     end
   end
