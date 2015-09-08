c3f5645858eca74e706fca8fd980804a257880ca
Luis Mondesi <lemsx1@gmail.com>
2015-09-08 04:52:47 +0000

    implements simple watch for directories

--- lib/god/inotify/watch_directory.rb
+++ lib/god/inotify/watch_directory.rb
@@ -3,49 +3,44 @@ require 'rb-inotify'
 module God
   module Inotify
     class WatchDirectory
+      attr_accessor :root
       def initialize(opts={})
         @notifier = INotify::Notifier.new
         @god = opts[:god]
+        @root = '/'
       end
-      # runs a loop to watch over all configured directories for changes
+      # configures notifier
       def watch_directories
-        return unless @god.respond_to? :watch and @god.respond_to? :unwatch
-
-        # parse YAML
-        @notifier.watch "/etc/god/processes", :create,
+        return unless @god.respond_to? :watch and
+          @god.respond_to? :unwatch
+        @notifier.watch File.join(@root,"etc","god","processes"), :create,
           :moved_to, :moved_from, :delete do |event|
-          case event.name
-          when "delete"
-            @god.unwatch do
-            end
-          else
-            @god.watch do
-            end
-          end
+          handle_event event
         end
-
-        # god native file format
-        @notifier.watch "/etc/god/conf.d", :create,
+        @notifier.watch File.join(@root,"etc","god","conf.d"), :create,
           :moved_to, :moved_from, :delete do |event|
-          case event.name
-          when "delete"
-            # TODO reload all?
-            @god.unwatch do
-            end
-          else
-            @god.watch do
-            end
-          end
+          handle_event event
         end
       end
+      # runs a loop to process all events on configured directories
       def run
         @notifier.run
       end
+      # process all events for configured directories
       def process
         @notifier.process
       end
       def stop
         @notifier.stop
+      end
+      private
+      def handle_event(event)
+        if event.flags.include? :delete or
+          event.flags == [:move_from, :move]
+          @god.unwatch event.absolute_name
+        else
+          @god.watch event.absolute_name
+        end
       end
     end
   end
